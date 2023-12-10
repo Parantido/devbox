d9ca0d939b63b996e5ea25e1050103ad7647ec3d
Parantido <parantido@techfusion.it>
2023-12-10 14:15:43 +0000

    Pushing initial commit

---
+++ containers_build/code-space/bin/boot_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/bin/code-server --bind-addr 0.0.0.0:8443 --user-data-dir /config/data --extensions-dir /config/extensions --disable-telemetry --auth password /config/workspace
+
+# Just for debug
+/usr/local/bin/looper.sh
