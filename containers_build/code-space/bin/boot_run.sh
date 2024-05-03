55bdd38880e97b3944f28d1d833c95abde948e43
Parantido <parantido@techfusion.it>
2024-05-03 14:28:15 +0000

    Code Server Image Updated

--- containers_build/code-space/bin/boot_run.sh
+++ containers_build/code-space/bin/boot_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Run code Server
 /usr/bin/code-server --bind-addr 0.0.0.0:8443 --user-data-dir /config/data --extensions-dir /config/extensions --disable-telemetry --auth password /config/workspace
 
 # Just for debug
