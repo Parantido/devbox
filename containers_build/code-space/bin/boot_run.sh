07748dab87bd8c619d4b947f633988712e142570
Parantido <parantido@techfusion.it>
2024-12-31 21:59:42 +0000

    Updating code space

--- containers_build/code-space/bin/boot_run.sh
+++ containers_build/code-space/bin/boot_run.sh
@@ -4,4 +4,4 @@
 /usr/bin/code-server --bind-addr 0.0.0.0:8443 --user-data-dir /config/data --extensions-dir /config/extensions --disable-telemetry --auth password /config/workspace
 
 # Just for debug
-/usr/local/bin/looper.sh
+# /usr/local/bin/looper.sh
